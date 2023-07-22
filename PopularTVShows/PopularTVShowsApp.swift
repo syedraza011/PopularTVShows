a5f75537398a0ef77695ee883c0a7ba597a49af0
Syed Raza <syed.raza011@gmail.com>
2023-07-22 11:29:12 -0400

    Initial Commit

---
+++ PopularTVShows/PopularTVShowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularTVShowsApp.swift
+//  PopularTVShows
+//
+//  Created by Syed Raza on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PopularTVShowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
